fce1c17075303356901cdaf100c5b5d32e7b4068
gabrielamoniz <monizdea@sheridancollege.ca>
2023-02-08 11:12:00 -0500

    Version 2

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -1,7 +1,7 @@
 /**
  * SYST 17796 Project Base code.
  * Students can modify and extend to implement their game.
- * Add your name as an author and the date!
+ * author @gabrielamoniz Feb 8, 2023
  */
 package ca.sheridancollege.project;
 
@@ -13,6 +13,23 @@ package ca.sheridancollege.project;
  */
 public abstract class Card {
     //default modifier for child classes
+    
+    private int value;
+    private String suit;
+    
+    public int getValue(){
+        return this.value;
+    }
+    public void setValue(int value){
+        this.value = value;
+    }
+    public String getSuit(){
+        return this.suit;
+    }
+    public void setSuit(String suit){
+        this.suit = suit;
+    }
+    
 
     /**
      * Students should implement this method for their specific children classes
@@ -20,6 +37,6 @@ public abstract class Card {
      * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
      */
     @Override
-    public abstract String toString();
+  public abstract String toString();
 
 }
---
+++ src/ca/sheridancollege/project/Class.java
@@ -0,0 +1,20 @@
+package ca.sheridancollege.project;
+
+public class Class {
+
+	private int attribute;
+
+	public void getAttribute() {
+		// TODO - implement Class.getAttribute
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param attribute
+	 */
+	public void setAttribute(int attribute) {
+		this.attribute = attribute;
+	}
+
+}
